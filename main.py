34eaf306b4e66a3b0b3d118e330af1fce141d6df
hey-akhil <akhil.pandeyy1@gmail.com>
2025-08-21 13:07:43 +0530

    donee

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
     uvicorn.run(app, host="0.0.0.0", port=7000)
     uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=1000)
+    uvicorn.run(app, host="0.0.0.0", port=1100)
 
